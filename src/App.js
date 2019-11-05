c6e585055ac96ff41670c90e2b103defba57fd51
AbdelIdir <homeworkoracle@protonmail.com>
2019-11-05 17:02:32 +0100

    Finished the MVP, and started stretch. Added styling to all the different buttons: Numbers, Operators, and Specials

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { numbers, operators, specials } from "./data";
 import Logo from "./components/DisplayComponents/Logo";
-import { NumberButton } from "./components/ButtonComponents/NumberButtons/NumberButton";
-import { OperatorButton } from "./components/ButtonComponents/OperatorButtons/OperatorButton";
-import { Display } from "./components/DisplayComponents/Display";
-console.log (NumberButton);
+import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Display from "./components/DisplayComponents/Display";
 
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
+
 // Logo has already been provided for you. Do the same for the remaining components
-
-
-
-
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -21,16 +19,19 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const [numberState, setNumberState] = useState("");
+  const [totals, setTotals] = useState("");
 
   return (
     <div className="container">
       <Logo />
+      <Display numberState={numberState} />
       <div className="App">
-        <Display />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Specials specials={specials} />
+        <Operators operators={operators} />
+        <Numbers numbers={numbers} setNumberState={setNumberState} />
       </div>
-      
-      
     </div>
   );
 }
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import SpecialButton from "./SpecialButton";
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Specials = () => {
+const Specials = props => {
   // STEP 2 - add the imported data to state
 
   return (
@@ -12,6 +12,11 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {props.specials.map(spec => {
+        return <SpecialButton specials={spec} />;
+      })}
     </div>
   );
 };
+
+export default Specials;
